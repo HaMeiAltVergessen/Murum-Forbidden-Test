011c7fc16148abd748a6d87322041970eacd682d
HaMaiAltVergessen <sebastian-raabe@hotmail.de>
2026-03-15 23:44:27 +0100

    stuf fix 9999

--- player/murum_animator.gd
+++ player/murum_animator.gd
@@ -152,22 +152,21 @@ func _play_attack_stretch() -> void:
 	if _attack_stretch_tween and _attack_stretch_tween.is_valid():
 		_attack_stretch_tween.kill()
 
-	var base_scale_x: float = absf(staff_sprite.scale.x)
-	var base_scale_y: float = absf(staff_sprite.scale.y)
-	var stretch_x: float = base_scale_x * 1.25  # 25% breiter
-	var stretch_y: float = base_scale_y * 0.85  # 15% flacher (squash)
+	var base_x: float = absf(staff_sprite.scale.x)  # 0.142
+	var base_y: float = absf(staff_sprite.scale.y)  # 0.142
 	var sign_x: float = 1.0 if facing_right else -1.0
 
+	# Subtiler Stretch: nur 8% in Y (Laenge), 5% squash in X
+	var stretch_y: float = base_y * 1.08
+	var squash_x: float = base_x * 0.95
+
 	_attack_stretch_tween = create_tween()
-	# Schnell strecken
+	# Schnell strecken (Stab wird laenger)
 	_attack_stretch_tween.tween_property(staff_sprite, "scale",
-		Vector2(stretch_x * sign_x, stretch_y), 0.06)
-	# Zurueck federn (leichtes Overshoot)
+		Vector2(squash_x * sign_x, stretch_y), 0.05)
+	# Zurueck federn
 	_attack_stretch_tween.tween_property(staff_sprite, "scale",
-		Vector2(base_scale_x * 0.95 * sign_x, base_scale_y * 1.05), 0.08)
-	# Normal + is_attacking zuruecksetzen
-	_attack_stretch_tween.tween_property(staff_sprite, "scale",
-		Vector2(base_scale_x * sign_x, base_scale_y), 0.1)
+		Vector2(base_x * sign_x, base_y), 0.12).set_trans(Tween.TRANS_ELASTIC).set_ease(Tween.EASE_OUT)
 	_attack_stretch_tween.tween_callback(func(): is_attacking = false)
 
 func _reset_staff_stretch() -> void:
